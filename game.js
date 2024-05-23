3a30d3167f5a86eaf2d74e5f238c5d12007f1b34
Rgjmerc <lvergara@brooklynsteamcenter.org>
2024-05-23 14:54:52 -0400

    working buttons

--- game.js
+++ game.js
@@ -4,7 +4,12 @@ let dynamite = 0
 let dynamiteAmmount = document.getElementById("dynamite")
 let knife = 0
 let knifeAmmount = document.getElementById("knife")
-
+let bomb = 0
+let bombAmmount = document.getElementById("bomb")
+let missile = 0
+let missileAmmount = document.getElementById("missile")
+let nuke = 0
+let nukeAmmount = document.getElementById("nuke")
 
 function increasePoints() {
     points = points + 1
@@ -17,7 +22,7 @@ function purchaseKnife() {
         knife = knife + 1
         knifeAmmount.innerText = knife + " Knives Owned"
     } else {
-        alert("Not enough deaths! You need at least 10 deaths to buy a knife")
+        alert("Not enough deaths! You need at least 10 deaths to buy a Knife")
     }
 }
 function purchaseDynamite() {
@@ -27,6 +32,41 @@ function purchaseDynamite() {
         dynamite = dynamite + 1
         dynamiteAmmount.innerText = dynamite + " Dynamite Owned"
     } else {
-        alert("Not enough deaths! You need at least 50 deaths to buy dynamite")
+        alert("Not enough deaths! You need at least 50 deaths to buy Dynamite")
     }
 }
+function purchaseBomb() {
+    if (points >= 250) {
+        points = points - 250
+        tracker.innerText = points + " Deaths"
+        bomb = bomb + 1
+        bombAmmount.innerText = bomb + " Bombs Owned"
+    } else {
+        alert("Not enough deaths! You need at least 250 deaths to buy Bombs")
+    }
+}
+function purchaseMissile() {
+    if (points >= 500) {
+        points = points - 500
+        tracker.innerText = points + " Deaths"
+        missile = missile + 1
+        missileAmmount.innerText = missile + " Missiles Owned"
+    } else {
+        alert("Not enough deaths! You need at least 500 deaths to buy Missiles")
+    }
+}
+function purchaseNuke() {
+    if (points >= 1000) {
+        points = points - 1000
+        tracker.innerText = points + " Deaths"
+        nuke = nuke + 1
+        nukeAmmount.innerText = nuke + " Nukes Owned"
+    } else {
+        alert("Not enough deaths! You need at least 10000 deaths to buy Nukes")
+    }
+}
+function gameLoop() {
+    points = points + (0.2 * knife)
+    tracker.innerText = points.toFixed(2) + " Deaths"
+}
+setInterval(gameLoop, 1000)
