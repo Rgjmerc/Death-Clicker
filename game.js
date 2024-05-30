917b5e7b1919328d0bb873bbe907f3cb2b03edd4
Rgjmerc <lvergara@brooklynsteamcenter.org>
2024-05-30 14:47:24 -0400

    Disabled Buttons

--- game.js
+++ game.js
@@ -77,8 +77,8 @@ function purchaseMissile() {
 }
 function purchaseNuke() {
     nCost = 1000 + nuke ** 4.25
-    if (points >= nukeCost) {
-        points = points - nukeCost
+    if (points >= nCost) {
+        points = points - nCost
         tracker.innerText = points.toFixed(1) + " Deaths"
         nuke = nuke + 1
         nukeAmmount.innerText = nuke + " Nukes Owned"
@@ -89,8 +89,36 @@ function purchaseNuke() {
 }
 function gameLoop() {
     points = points + (1 * dynamite) + (5 * bomb) + (10 * missile) + (25 * nuke) 
-    cps = (1 * dynamite) + (5 * bomb) + (10 * missile) + (25 * nuke) 
+}
+function checker() {
     tracker.innerText = points.toFixed(1) + " Deaths"
+    cps = (1 * dynamite) + (5 * bomb) + (10 * missile) + (25 * nuke) 
     cpsAmmount.innerText = cps + " deaths per second"
+    if (points < 10 + knife ** 2.25) {
+        document.getElementById("knifebtn").disabled = true;
+    }else{
+        document.getElementById("knifebtn").disabled = false;
+    }
+    if (points < 50 + dynamite ** 2.75) {
+        document.getElementById("dynamitebtn").disabled = true;
+    }else{
+        document.getElementById("dynamitebtn").disabled = false;
+    }
+    if (points < 250 + bomb ** 3.25) {
+        document.getElementById("bombbtn").disabled = true;
+    }else{
+        document.getElementById("bombbtn").disabled = false;
+    }
+    if (points < 500 + missile ** 3.75) {
+        document.getElementById("missilebtn").disabled = true;
+    }else{
+        document.getElementById("missilebtn").disabled = false;
+    }
+    if (points < 1000 + nuke ** 4.25) {
+        document.getElementById("nukebtn").disabled = true;
+    }else{
+        document.getElementById("nukebtn").disabled = false;
+    }
 }
 setInterval(gameLoop, 1000)
+setInterval(checker, 1)
